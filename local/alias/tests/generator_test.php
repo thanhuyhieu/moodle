95c1942947034d333d2ce90673fa2d509e43b80e
Hieu Than <thanhuyhieu@gmail.com>
2021-06-16 16:55:46 +0700

    Fixed - Added PHPUnit

--- local/alias/tests/generator_test.php
+++ local/alias/tests/generator_test.php
@@ -34,24 +34,48 @@
 class local_alias_generator_testcase extends advanced_testcase {
 
     public function test_create_instance() {
-        global $DB, $SITE;
+        global $DB;
         $this->resetAfterTest();
         $this->setAdminUser();
         
-        $generator = $this->getDataGenerator()->get_plugin_generator('local_alias');
+        // $generator = $this->getDataGenerator()->get_plugin_generator('local_alias');
 
         $params = array('friendly' => 'http://example.com/home', 'destination' => 'http://example.com/okay?id=1');
-        $alias = $generator->create_instance(array('course' => $SITE->id), $params);
-        $records = $DB->get_records('alias', array('id' => $alias->id), 'id');
+        // $alias = $generator->create_instance($params);
+        
+        $alias_id = $DB->insert_record('alias', $params);
+        $records = $DB->get_records('alias', array('id' => $alias_id),'','id');
         $this->assertEquals(1, count($records));
-        $this->assertTrue(array_key_exists($alias->id, $records));
+        $this->assertTrue(array_key_exists($alias_id, $records));
 
         $params = array('friendly' => 'http://example.com/modhome', 'destination' => 'http://example.com/mod?id=1');
-        $alias = $generator->create_instance(array('course' => $SITE->id), $params);
-        $records = $DB->get_records('alias', array('id' => $alias->id), '*');
+        // $generator = $this->getDataGenerator()->get_plugin_generator('local_alias');
+        // $alias = $generator->create_instance($params);
+        $alias_id = $DB->insert_record('alias', $params);
+        $records = $DB->get_records('alias');
         $this->assertEquals(2, count($records));
-        $this->assertEquals('http://example.com/modhome', $records[$alias->id]->friendly);
-        $this->assertEquals('http://example.com/mod?id=1', $records[$alias->id]->destination);
+        $this->assertEquals('http://example.com/modhome', $records[$alias_id]->friendly);
+        $this->assertEquals('http://example.com/mod?id=1', $records[$alias_id]->destination);
+    }
+
+    public function test_updating() {
+        global $DB;
+        $this->resetAfterTest();
+        $this->setAdminUser();
+
+        $params = array('friendly' => 'http://example.com/home', 'destination' => 'http://example.com/okay?id=1');
+        $alias_id = $DB->insert_record('alias', $params);
+        $records = $DB->get_records('alias', array('id' => $alias_id),'','id');
+        $this->assertEquals(1, count($records));
+        $this->assertTrue(array_key_exists($alias_id, $records));
+
+        $params_update = array('id' => $alias_id, 'friendly' => 'http://example.com/updating', 'destination' => 'http://example.com/updating?id=1');
+        $DB->update_record('alias', $params_update);
+        $records_update = $DB->get_records('alias', array('id' => $alias_id),'','*');
+        $this->assertEquals(1, count($records_update));
+        $this->assertEquals($alias_id, $records_update[$alias_id]->id);
+        $this->assertEquals('http://example.com/updating', $records_update[$alias_id]->friendly);
+        $this->assertEquals('http://example.com/updating?id=1', $records_update[$alias_id]->destination);
     }
 
     public function test_deleting() {
@@ -61,4 +85,5 @@ class local_alias_generator_testcase extends advanced_testcase {
         $DB->delete_records('alias');
         $this->assertEmpty($DB->get_records('alias'));
     }
+    
 }
